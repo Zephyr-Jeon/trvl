e620114bf82a39161b21d8703a3f53261823f512
Zephyr Jeon <rltjd1258@gmail.com>
2020-09-15 02:06:11 +0900

    Updates

---
+++ precache-manifest.b09ca43aeab3c5ac70f53f7328fc14a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2927cc87bcef1a6639c3dd1a5c035d3",
+    "url": "/trvl/index.html"
+  },
+  {
+    "revision": "066b01f7ed65eeb54472",
+    "url": "/trvl/static/css/main.f6f697d5.chunk.css"
+  },
+  {
+    "revision": "7d9c129e6bfdba0144ed",
+    "url": "/trvl/static/js/2.92469236.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trvl/static/js/2.92469236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066b01f7ed65eeb54472",
+    "url": "/trvl/static/js/main.47fae778.chunk.js"
+  },
+  {
+    "revision": "4cb96473a1e193c73c37",
+    "url": "/trvl/static/js/runtime-main.35e7fd7a.js"
+  }
+]);
